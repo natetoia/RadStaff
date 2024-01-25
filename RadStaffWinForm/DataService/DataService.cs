849daa5fc79e8d50d247d4db21bf6b62ce7b276f
Nathaniel Tohovaka <nathaniel.tohovaka@radfords.co.nz>
2024-01-25 21:52:08 +1300

    added deleted call, inverted a nullcheck in Update Method

--- RadStaffWinForm/DataService/DataService.cs
+++ RadStaffWinForm/DataService/DataService.cs
@@ -1,112 +1,120 @@
-﻿using System.Diagnostics;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using RadStaffWinForm.Models;
 
-namespace RadStaffWinForm.DataService
+namespace RadStaffWinForm.DataService;
+
+public class DataService
 {
-    public class DataService
+    public static List<StaffMember>? GetStaffMembersWithDetails(List<int> statusIds)
     {
-        public static List<StaffMember>? GetStaffMembersWithDetails(List<int> statusIds)
+        using (var context = new RadDbContext())
         {
-            using (var context = new RadDbContext())
-            {
-                return context.Set<StaffMember>()
-                    .Include(s => s.StaffStatus)
-                    .Include(s => s.StaffType)
-                    .Include(s => s.StaffManager)?
-                    .Where(s => statusIds.Contains(s.StaffStatusId))
-                    .OrderBy(s => s.StaffId)
-                    .ToList();
-            }
+            return context.Set<StaffMember>()
+                .Include(s => s.StaffStatus)
+                .Include(s => s.StaffType)
+                .Include(s => s.StaffManager)?
+                .Where(s => statusIds.Contains(s.StaffStatusId))
+                .OrderBy(s => s.StaffId)
+                .ToList();
         }
+    }
 
-        public static StaffMember? GetStaffMemberById(int staffId)
+    public static StaffMember? GetStaffMemberById(int staffId)
+    {
+        using (var context = new RadDbContext())
         {
-            using (var context = new RadDbContext())
-            {
-                return context.Set<StaffMember>()
-                    .Include(s => s.StaffStatus)
-                    .Include(s => s.StaffType)
-                    .Include(s => s.StaffManager)
-                    .FirstOrDefault(s => s.StaffId == staffId);
-            }
+            return context.Set<StaffMember>()
+                .Include(s => s.StaffStatus)
+                .Include(s => s.StaffType)
+                .Include(s => s.StaffManager)
+                .FirstOrDefault(s => s.StaffId == staffId);
         }
+    }
 
-        public static List<object> GetManagerStaffMembersIdAndName()
+    public static List<object> GetManagerStaffMembersIdAndName()
+    {
+        using (var context = new RadDbContext())
         {
-            using (var context = new RadDbContext())
-            {
-                var result = context.StaffMembers!
-                    .Where(s => s.StaffTypeId == 2)
-                    .OrderBy(s => s.StaffFirstName)
-                    .Select(s => new
-                    {
-                        StaffMemberId = s.StaffId,
-                        StaffMemberName = $"{s.StaffFirstName} {s.StaffLastName}"
-                    })
-                    .ToList();
-
-                return result.Cast<object>().ToList();
-            }
-        }
-
-        public static List<StaffType> GetStaffTypes()
-        {
-            using (var context = new RadDbContext())
-            {
-                return context.StaffTypes!.ToList();
-            }
-        }
-
-        public static List<StaffStatus> GetStaffStatuses()
-        {
-            using (var context = new RadDbContext())
-            {
-                return context.StaffStatuses!.ToList();
-            }
-        }
-
-
-        public static void AddStaffMember(StaffMember staffMember)
-        {
-            using (var context = new RadDbContext())
-            {
-                context.StaffMembers!.Add(staffMember);
-                context.SaveChanges();
-            }
-        }
-
-        public static void UpdateStaffMemberById(int staffId, StaffMember updatedStaffMember)
-        {
-            using (var context = new RadDbContext())
-            {
-                StaffMember? existingStaffMember = context.StaffMembers!
-                    .FirstOrDefault(s => s.StaffId == staffId);
-                if (existingStaffMember != null)
+            var result = context.StaffMembers!
+                .Where(s => s.StaffTypeId == 2)
+                .OrderBy(s => s.StaffFirstName)
+                .Select(s => new
                 {
-                    existingStaffMember.StaffTitle = updatedStaffMember.StaffTitle;
-                    existingStaffMember.StaffFirstName = updatedStaffMember.StaffFirstName;
-                    existingStaffMember.StaffMiddleInitial = updatedStaffMember.StaffMiddleInitial;
-                    existingStaffMember.StaffLastName = updatedStaffMember.StaffLastName;
-                    existingStaffMember.StaffIrdnumber = updatedStaffMember.StaffIrdnumber;
-                    existingStaffMember.StaffCellPhone = updatedStaffMember.StaffCellPhone;
-                    existingStaffMember.StaffHomePhone = updatedStaffMember.StaffHomePhone;
-                    existingStaffMember.StaffOfficeExtension = updatedStaffMember.StaffOfficeExtension;
-                    existingStaffMember.StaffTypeId = updatedStaffMember.StaffTypeId;
-                    existingStaffMember.StaffManagerId = updatedStaffMember.StaffManagerId;
-                    existingStaffMember.StaffStatusId = updatedStaffMember.StaffStatusId;
-                }
-                context.SaveChanges();
-            }
+                    StaffMemberId = s.StaffId,
+                    StaffMemberName = $"{s.StaffFirstName} {s.StaffLastName}"
+                })
+                .ToList();
 
+            return result.Cast<object>().ToList();
         }
+    }
 
-        public static bool HasStaffMembersWithManagerId(int managerId)
+    public static List<StaffType> GetStaffTypes()
+    {
+        using (var context = new RadDbContext())
         {
-            using (var context = new RadDbContext())
-            {
-                return context.StaffMembers!.Any(s => s.StaffManagerId == managerId);
-            }
+            return context.StaffTypes!.ToList();
+        }
+    }
+
+    public static List<StaffStatus> GetStaffStatuses()
+    {
+        using (var context = new RadDbContext())
+        {
+            return context.StaffStatuses!.ToList();
+        }
+    }
+
+
+    public static void AddStaffMember(StaffMember staffMember)
+    {
+        using (var context = new RadDbContext())
+        {
+            context.StaffMembers!.Add(staffMember);
+            context.SaveChanges();
+        }
+    }
+
+    public static void UpdateStaffMemberById(int staffId, StaffMember updatedStaffMember)
+    {
+        using (var context = new RadDbContext())
+        {
+            var existingStaffMember = context.StaffMembers!
+                .FirstOrDefault(s => s.StaffId == staffId);
+            if (existingStaffMember == null) return;
+            existingStaffMember.StaffTitle = updatedStaffMember.StaffTitle;
+            existingStaffMember.StaffFirstName = updatedStaffMember.StaffFirstName;
+            existingStaffMember.StaffMiddleInitial = updatedStaffMember.StaffMiddleInitial;
+            existingStaffMember.StaffLastName = updatedStaffMember.StaffLastName;
+            existingStaffMember.StaffIrdnumber = updatedStaffMember.StaffIrdnumber;
+            existingStaffMember.StaffCellPhone = updatedStaffMember.StaffCellPhone;
+            existingStaffMember.StaffHomePhone = updatedStaffMember.StaffHomePhone;
+            existingStaffMember.StaffOfficeExtension = updatedStaffMember.StaffOfficeExtension;
+            existingStaffMember.StaffTypeId = updatedStaffMember.StaffTypeId;
+            existingStaffMember.StaffManagerId = updatedStaffMember.StaffManagerId;
+            existingStaffMember.StaffStatusId = updatedStaffMember.StaffStatusId;
+            context.SaveChanges();
+        }
+    }
+
+    public static bool HasStaffMembersWithManagerId(int managerId)
+    {
+        using (var context = new RadDbContext())
+        {
+            return context.StaffMembers!.Any(s => s.StaffManagerId == managerId);
+        }
+    }
+
+    public static void DeleteStaffMemberById(int staffId)
+    {
+        using (var context = new RadDbContext())
+        {
+            var staffMemberToDelete = context.StaffMembers!
+                .FirstOrDefault(s => s.StaffId == staffId);
+
+            if (staffMemberToDelete == null) return;
+            context.StaffMembers!.Remove(staffMemberToDelete);
+            context.SaveChanges();
         }
     }
 }
